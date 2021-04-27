eff01fcf1e614b14ebbf462a7ae0026105aa8745
Ashutosh Singh <ashutoshsingh.0207@gmail.com>
2021-04-27 22:44:42 +0530

    remember example for stage management

--- app/src/main/java/com/example/counter/MainActivity.kt
+++ app/src/main/java/com/example/counter/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.counter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material.Button as Button1
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent{Counter()}
+    }
+
+    @Composable
+    fun Counter(){
+        val count  = remember {
+            mutableStateOf(0)
+        }
+
+        Button1(onClick = { count.value++ }) { Text("Clicked ${count.value.toString()} times!")
+        }
+
+    }
+
+    @Preview
+    @Composable
+    fun MainUI(){
+        Counter()
     }
 }
