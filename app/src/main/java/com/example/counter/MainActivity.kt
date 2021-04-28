466711ea00c7bf13231efd71d95f22feefb65643
Ashutosh Singh <ashutoshsingh.0207@gmail.com>
2021-04-28 10:31:36 +0530

    UI fixes

--- app/src/main/java/com/example/counter/MainActivity.kt
+++ app/src/main/java/com/example/counter/MainActivity.kt
@@ -3,33 +3,48 @@ package com.example.counter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.material.Button as Button1
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent{Counter()}
+        setContent { Counter() }
     }
 
     @Composable
-    fun Counter(){
-        val count  = remember {
+    fun Counter() {
+        val count = remember {
             mutableStateOf(0)
         }
 
-        Button1(onClick = { count.value++ }) { Text("Clicked ${count.value.toString()} times!")
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button1(onClick = { count.value++ }) {
+                Text("Clicked ${count.value.toString()} times!")
+            }
         }
 
     }
 
     @Preview
     @Composable
-    fun MainUI(){
+    fun MainUI() {
         Counter()
     }
 }
