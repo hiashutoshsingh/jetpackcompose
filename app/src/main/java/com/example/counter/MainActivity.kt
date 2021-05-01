51aa1a51b36ebe4e390115909a0523c5284da4fe
Ashutosh Singh <ashutoshsingh.0207@gmail.com>
2021-05-01 21:11:46 +0530

    simple state management

--- app/src/main/java/com/example/counter/MainActivity.kt
+++ app/src/main/java/com/example/counter/MainActivity.kt
@@ -10,24 +10,36 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.material.Button as Button1
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { Counter() }
+        setContent { Content() }
     }
 
     @Composable
-    fun Counter() {
-        val count = remember {
-            mutableStateOf(0)
-        }
+    fun Content() {
 
+        /// managing state using remember
+        /// state is managed, but not when configuration changes
+        //        val count = remember {
+        //            mutableStateOf(0)
+        //        }
+
+        /// managing state using rememberSaveable
+        /// state is managed even if configuration changes
+        var count by rememberSaveable { mutableStateOf(0) }
+        Counter(count, onValueChange = {  count })
+    }
+
+    @Composable
+    fun Counter(count: Int, onValueChange: (Int) -> Unit) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
@@ -35,16 +47,12 @@ class MainActivity : ComponentActivity() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Button1(onClick = { count.value++ }) {
-                Text("Clicked ${count.value.toString()} times!")
+            Button1(onClick = { onValueChange(count+1) } ) {
+                Text("Clicked $count times!")
             }
         }
 
     }
 
-    @Preview
-    @Composable
-    fun MainUI() {
-        Counter()
-    }
 }
+
